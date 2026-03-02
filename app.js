8d1bd157729e53f254df61b8bc5d9999d8e2cbdb
Jovan Bergh <jovanbergh17@gmail.com>
2026-03-02 15:01:38 -0700

    Established error handlers & corrected app route configuration

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const cors = require("cors"); // used for API distribution
 const productsRouter = require("./api/products/products.router");
+const errorHandler = require("./api/errors/errorHandler");
+const notFound = require("./api/errors/notFound");
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
-app.use("/", () => (req, res) => {
-  res.json({ message: "Hello Render!" });
-});
+
 
 app.use("/api/products", productsRouter);
 
+app.use(notFound);
 
+app.use(errorHandler);
+
+app.use("", (req, res) => {
+  res.json({ message: "Hello Render!" });
+});
 module.exports = app;
